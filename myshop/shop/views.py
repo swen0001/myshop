0fe7faf9d23d82f1ae9683dcee8a6f4c9164d31d
swen0001 <oleksandr.venger0001@gmail.com>
2023-02-05 00:42:16 +0200

    Added sorting by ('-created')

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -6,11 +6,11 @@ from cart.forms import CartAddProductForm
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.objects.filter(available=True).order_by('-created')
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
-        products = products.filter(category=category)
+        products = products.filter(category=category).order_by('-created')
     return render(request,
                   'shop/product/list.html',
                   {'category': category, 'categories': categories, 'products': products})
